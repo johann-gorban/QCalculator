4e4390b37399ff2a3cfa01bdaa627b66c063c140
johann-gorban <ivangorbanwork@gmail.com>
2025-04-26 22:54:57 +0300

    architecture init

---
+++ source/calculator.cpp
@@ -0,0 +1,11 @@
+#include "calculator.hpp"
+
+std::string Calculator::to_postfix(const std::string &str) const {
+    std::string expression = "";
+}
+
+
+std::string Calculator::get_num_from_str(const std::string &str, std::size_t &pos) const noexcept {
+    std::string number = "";
+    
+}
