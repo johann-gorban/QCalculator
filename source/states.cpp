83d7b85ebcbdb21f51a8164001a146bed359238c
johann-gorban <ivangorbanwork@gmail.com>
2025-04-29 19:35:31 +0300

    start state implemented

---
+++ source/states.cpp
@@ -0,0 +1,28 @@
+#include "states.hpp"
+
+#include <memory>
+
+void StartState::handle_char(TokenizerContext& context, char c) {
+    std::unique_ptr<State> new_state;
+    // Pay attention to possible bug here (because of start with .) 
+    if (std::isdigit(c) || c == '.') {
+        new_state = std::make_unique<NumberState>();
+    }
+    else if (std::isalpha(c)) {
+        new_state = std::make_unique<IdentifierState>();
+    }
+    else if (std::string("+-^*/").find(c) != std::string::npos) {
+        new_state = std::make_unique<OperatorState>();
+    }
+    // Pay attention to left paranthesis in the beginning
+    // Should throw a syntax error
+    else if (c == ')' || c == '(') {
+        new_state = std::make_unique<ParanthesisState>();
+    }
+    // Pay attention to comma in the beginning of expression
+    // Should throw a syntax error
+    else if (c == ',') {
+        new_state = std::make_unique<CommaState>();
+    }
+
+}
