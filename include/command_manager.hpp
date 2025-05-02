6e22777b31d86929edc84fd76df94929fde2e37d
johann-gorban <ivangorbanwork@gmail.com>
2025-05-02 19:06:42 +0300

    tokens moved to token manager

--- include/command_manager.hpp
+++ include/command_manager.hpp
@@ -9,13 +9,10 @@
 class CommandManager {
 private:
     std::unordered_map<std::string, std::shared_ptr<Command>> commands;
-    std::unordered_map<std::string, std::shared_ptr<Token>> tokens;
 public:
     CommandManager();
 
     const std::shared_ptr<Command> get_command(const std::string &token_name) const;
-
-    bool has_token(const std::string &token_name) const;
 
     bool has_command(const std::string &token_name) const;
 };
--- source/command_manager.cpp
+++ source/command_manager.cpp
@@ -2,38 +2,22 @@
 
 
 CommandManager::CommandManager() {
-    commands["+"] = std::make_shared<AdditionCommand>();
-    tokens["+"] = std::make_shared<OperatorAddition>();
-
-    commands["-"] = std::make_shared<SubstractCommand>();
-    tokens["-"] = std::make_shared<OperatorSubstraction>();
-
-    commands["*"] = std::make_shared<MultiplyCommand>();
-    tokens["*"] = std::make_shared<OperatorMultiplication>();
-
-    commands["/"] = std::make_shared<DivisionCommand>();
-    tokens["/"] = std::make_shared<OperatorDivision>();
-
-    commands["^"] = std::make_shared<PowerCommand>();
-    tokens["^"] = std::make_shared<OperatorPower>();
-
-    // Fucking unary minus 
-    commands["~"] = std::make_shared<MinusCommand>();
-    tokens["~"] = std::make_shared<OperatorMinus>();
+    this->commands["+"] = std::make_shared<AdditionCommand>();
+    this->commands["-"] = std::make_shared<SubstractCommand>();
+    this->commands["*"] = std::make_shared<MultiplyCommand>();
+    this->commands["/"] = std::make_shared<DivisionCommand>();
+    this->commands["^"] = std::make_shared<PowerCommand>();
+    this->commands["~"] = std::make_shared<MinusCommand>();
 }
 
 const std::shared_ptr<Command> CommandManager::get_command(const std::string &token_name) const {
     std::shared_ptr<Command> command(nullptr);
 
-    if (commands.find(token_name) != commands.end()) {
-        command = commands.at(token_name);
+    if (this->has_command(token_name)) {
+        command = this->commands.at(token_name);
     }
     
     return command;
-}
-
-bool CommandManager::has_token(const std::string &token_name) const {
-    return (tokens.find(token_name) != tokens.end());
 }
 
 bool CommandManager::has_command(const std::string &token_name) const {
