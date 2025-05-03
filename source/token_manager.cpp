64f4a60c8f12235a83d6ca97166447ff2bcf2d18
johann-gorban <ivangorbanwork@gmail.com>
2025-05-03 16:35:06 +0300

    unary minus removed

--- source/token_manager.cpp
+++ source/token_manager.cpp
@@ -2,23 +2,37 @@
 
 
 TokenManager::TokenManager() {
-    this->tokens["+"] = std::make_shared<OperatorAddition>();
-    this->tokens["-"] = std::make_shared<OperatorSubstraction>();
-    this->tokens["*"] = std::make_shared<OperatorMultiplication>();
-    this->tokens["/"] = std::make_shared<OperatorDivision>();
-    this->tokens["^"] = std::make_shared<OperatorPower>();
-    this->tokens["~"] = std::make_shared<OperatorMinus>();
+    // Operator tokens
+    this->operator_tokens["+"] = std::make_shared<OperatorAddition>();
+    this->operator_tokens["-"] = std::make_shared<OperatorSubstraction>();
+    this->operator_tokens["*"] = std::make_shared<OperatorMultiplication>();
+    this->operator_tokens["/"] = std::make_shared<OperatorDivision>();
+    this->operator_tokens["^"] = std::make_shared<OperatorPower>();
+
+    // Function tokens 
 }
 
-bool TokenManager::has_token(const std::string &name) const {
-    return (this->tokens.find(name) != this->tokens.end());
+bool TokenManager::function_has_token(const std::string &name) const {
+    return (this->function_tokens.find(name) != this->function_tokens.end());
+}
+
+bool TokenManager::operator_has_token(char name) const {
+    // Convert char to string
+    std::string op_name;
+    op_name += name;
+
+    return (this->function_tokens.find(op_name) != this->function_tokens.end());
 }
 
 const std::shared_ptr<Token> TokenManager::get_token(const std::string &name) const {
     std::shared_ptr<Token> token_ptr(nullptr);
     
-    if (this->has_token(name)) {
-        token_ptr = this->tokens.at(name);
+    if (this->operator_has_token(name.at(0))) {
+        token_ptr = this->operator_tokens.at(name);
+    }
+
+    else if (this->function_has_token(name)) {
+        token_ptr = this->function_tokens.at(name);
     }
 
     return token_ptr;
