9510b2a274bb6a21f8e7be3d1f8293edb6327759
johann-gorban <ivangorbanwork@gmail.com>
2025-05-03 17:04:00 +0300

    Number state redo, binary operator state implemented

--- source/states.cpp
+++ source/states.cpp
@@ -1,76 +1,189 @@
 #include "states.hpp"
 #include "lexer_context.hpp"
+#include "token_manager.hpp"
 
 #include <memory>
 #include <algorithm>
 #include <stdexcept>
 
 
-void State::finish_input(LexerContext &context) {
-    // Empty as template
+void State::emit_token_from_buffer(LexerContext &context) {
+    // Nothing here
 }
 
 
 void StartState::handle_char(LexerContext &context, char c) {
-    std::unique_ptr<State> new_state;
-    // Pay attention to possible bug here (because of start with .) 
+    state_ptr new_state;
+
     if (std::isdigit(c) || c == '.') {
-        new_state = std::make_unique<NumberState>();
-    }
-    else if (std::isalpha(c)) {
-        new_state = std::make_unique<IdentifierState>();
-    }
-    else if (std::string("+-^*/").find(c) != std::string::npos) {
-        new_state = std::make_unique<OperatorState>();
-    }
-    // Pay attention to left paranthesis in the beginning
-    // Should throw a syntax error
-    else if (c == ')' || c == '(') {
-        new_state = std::make_unique<ParanthesisState>();
-    }
-    // Pay attention to comma in the beginning of expression
-    // Should throw a syntax error
-    else if (c == ',') {
-        new_state = std::make_unique<CommaState>();
+        new_state = std::make_shared<NumberState>();
     }
 
+    else if (std::isalpha(c)) {
+        new_state = std::make_shared<IdentifierState>();
+    }
+
+    // Handle unary minus in the beginning
+    else if (c == '-') {
+        new_state = std::make_shared<UnaryMinusState>();
+    }
+
+    else if (c == '(') {
+        new_state = std::make_shared<LeftParanthesisState>();
+    }
+    
+    else if (c == ')') {
+        new_state = std::make_shared<RightParanthesisState>();
+    }
+
+    else if (c == ',') {
+        new_state = std::make_shared<CommaState>();
+    }
+
+    else {
+        TokenManager token_manager;
+        if (token_manager.operator_has_token(c)) {
+            new_state = std::make_shared<OperatorState>();
+        }
+    }
+
+    context.set_state(new_state);
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////
 
 void NumberState::handle_char(LexerContext &context, char c) {
-    std::unique_ptr<State> new_state;
+    state_ptr new_state = std::make_shared<NumberState>();
+
     if (std::isdigit(c)) {
         context.append_buffer(c);
-        new_state = std::make_unique<NumberState>(); // Why?
     }
+
     else if (c == '.') {
         auto current_buffer = context.get_buffer();
+        
+        // Need to check how much floating points are in the number
+        // If there is a point and now there is a new point, it is lexical error
+
         if (std::count(current_buffer.begin(), current_buffer.end(), '.') != 0) {
-            throw std::runtime_error("Invalid float number format");
+            throw std::runtime_error("Lexical error: invalid float number format");
         }
         else {
             context.append_buffer(c);
-            new_state = std::make_unique<NumberState>(); // Why?
         }
     }
-    else if (std::string("+-^*/").find(c) != std::string::npos) {
-        new_state = std::make_unique<OperatorState>();
-    }
-    else if (c == ')' || c == '(') {
-        new_state = std::make_unique<NumberState>();
-    }
-    else if (std::isalpha(c)) {
-        throw std::runtime_error("Invalid number format"); // Maybe add ErrorState?
+
+    
+    else {
+        // Create token after the state changed
+        this->emit_token_from_buffer(context);
+
+        TokenManager token_manager;
+        if (token_manager.operator_has_token(c)) {
+            new_state = std::make_shared<OperatorState>();
+        }
+
+        if (c == ')') {
+            new_state = std::make_shared<RightParanthesisState>();
+        }
+
+        if (c == '(') {
+            new_state = std::make_shared<LeftParanthesisState>();
+            // Should also add multiplication token
+            // 2(2 + 2) <=> 2 * (2 + 2)
+        }
+    
+        if (std::isalpha(c)) {
+            new_state = std::make_shared<IdentifierState>();
+            // Should also add multiplication token
+            // 2sin(pi/2) <=> 2 * sin(pi/2)
+        }
     }
     
-    context.set_state(std::move(new_state));
+    context.set_state(new_state);
 }
 
-
-void NumberState::finish_input(LexerContext &context) {
+void NumberState::emit_token_from_buffer(LexerContext &context) {
+    // Get buffer that stores number
     auto current_buffer = context.get_buffer();
+    
+    // Convert string number to double
     double number = std::stod(current_buffer);
     
+    // Create new number token
     auto number_token = std::make_shared<Token>(Number(number));
-    context.emit_token(number_token);
+
+    // Append the created token to context
+    context.append_token(number_token);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void OperatorState::handle_char(LexerContext &context, char c) {
+    state_ptr new_state = std::make_shared<OperatorState>();
+
+    if (c == '-') {
+        new_state = std::make_shared<UnaryMinusState>();
+    }
+
+    else if (std::isdigit(c) || c == '.') {
+        new_state = std::make_shared<NumberState>();
+        this->emit_token_from_buffer(context);
+    }
+
+    else if (std::isalpha(c)) {
+        new_state = std::make_shared<IdentifierState>();
+        this->emit_token_from_buffer(context);
+    }
+
+    else if (c == ')') {
+        new_state = std::make_shared<LeftParanthesisState>();
+        this->emit_token_from_buffer(context);
+    }
+
+    else if (c == '(') {
+        new_state = std::make_shared<RightParanthesisState>();
+        this->emit_token_from_buffer(context);
+    }
+
+    else if (c == ',') {
+        new_state = std::make_shared<CommaState>();
+        this->emit_token_from_buffer(context);
+    }
+
+    else {
+        TokenManager token_manager;
+        if (token_manager.operator_has_token(c)) {
+            new_state = std::make_shared<OperatorState>();
+
+            context.append_buffer(c);
+            this->emit_token_from_buffer(context);
+        }
+    }
+
+    context.set_state(new_state);
+}
+
+void OperatorState::emit_token_from_buffer(LexerContext &context) {
+    // Get buffer that stores operator
+    auto current_buffer = context.get_buffer();
+    
+    // Get operator from buffer string
+    std::string op_name = current_buffer;
+
+    // Create token manager to define operator we got
+    TokenManager token_manager;
+
+    // Create new operator token
+    auto number_token = std::make_shared<Token>(token_manager.get_token(op_name));
+
+    // Append the created token to context
+    context.append_token(number_token);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void UnaryMinusState::handle_char(LexerContext &context, char c) {
+    // Will emit number token for -1 and multiplication token
+    // So appending token into context is needed
 }
