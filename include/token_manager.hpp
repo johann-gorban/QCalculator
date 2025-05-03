dfd03024624cb18b889bda945b671df039689015
johann-gorban <ivangorbanwork@gmail.com>
2025-05-03 16:34:54 +0300

    unary minus removed

--- include/token_manager.hpp
+++ include/token_manager.hpp
@@ -8,11 +8,14 @@
 
 class TokenManager {
 private:
-    std::unordered_map<std::string, std::shared_ptr<Token>> tokens;
+    static std::unordered_map<std::string, std::shared_ptr<Operator>> operator_tokens;
+    static std::unordered_map<std::string, std::shared_ptr<Function>> function_tokens;
 public:
     TokenManager();
 
     const std::shared_ptr<Token> get_token(const std::string &name) const;
 
-    bool has_token(const std::string &name) const;
+    bool function_has_token(const std::string &name) const;
+
+    bool operator_has_token(char operator_name) const;
 };
