8f220b656bbf213e84787949d3f5665cbfe40b6f
johann-gorban <ivangorbanwork@gmail.com>
2025-05-05 20:41:18 +0300

    Comments added

--- source/calculator.cpp
+++ source/calculator.cpp
@@ -5,7 +5,10 @@
 #include <stdexcept>
 
 double Calculator::calculate(std::vector<std::shared_ptr<Token>> tokens_rpn) {
+    // Stack for computation. There the intermediate calculations are being stored
     std::stack<double> computations;
+
+    // Command manager for getting command for calculation by token
     CommandManager command_manager;
 
     for (const auto &token: tokens_rpn) {
