e330196f8aae1a620494098b266c1c3e81c72204
johann-gorban <ivangorbanwork@gmail.com>
2025-05-10 18:52:32 +0300

    available token names type replaced to vector<string> from map<string, strin>

--- include/tokenization/manager.hpp
+++ include/tokenization/manager.hpp
@@ -1,20 +1,19 @@
 #pragma once
 
 #include <unordered_map>
+#include <vector>
 #include <string>
 
 #include "tokens.hpp"
 
 class TokenManager {
 private:
-    std::unordered_map<std::string, std::string> available_tokens;
+    std::vector<std::string> available_token_names;
     std::unordered_map<TokenType, std::string> tokentype_names;
 public:
     TokenManager();
 
     bool is_valid_name(const std::string &name) const noexcept;
-
-    const std::string &get_name(const std::string &name) const;
 
     const std::string &get_type_name(TokenType) const;
 };
--- source/tokenization/manager.cpp
+++ source/tokenization/manager.cpp
@@ -1,40 +1,33 @@
 #include "tokenization/manager.hpp"
 
 #include <stdexcept>
+#include <algorithm>
 
 TokenManager::TokenManager() {
     // Token types for debugging
     this->tokentype_names[TokenType::Number] = "NUMBER";
     this->tokentype_names[TokenType::Function] = "FUNCTION";
-    this->tokentype_names[TokenType::BinaryOperator] = "BINARY_OPERATOR";
-    this->tokentype_names[TokenType::UnaryOperator] = "UNARY_OPERATOR";
-    this->tokentype_names[TokenType::LeftParenthesis] = "LEFT_PARENTHESIS";
-    this->tokentype_names[TokenType::RightParenthesis] = "RIGHT_PARENTHESIS";
+    this->tokentype_names[TokenType::Operator] = "OPERATOR";
+    this->tokentype_names[TokenType::Parenthesis] = "PARENTHESIS";
     this->tokentype_names[TokenType::Separator] = "SEPARATOR";
 
-    // Operators
-    this->available_tokens["-"] = "SUB";
-    this->available_tokens["+"] = "ADD";
-    this->available_tokens["*"] = "MUL";
-    this->available_tokens["/"] = "DIV";
-    this->available_tokens["^"] = "POW";
+    this->available_token_names.push_back("-");
+    this->available_token_names.push_back("+");
+    this->available_token_names.push_back("*");
+    this->available_token_names.push_back("/");
+    this->available_token_names.push_back("^");
 
-    // Functions
-    this->available_tokens["sin"] = "SIN";
-    // ...
+    this->available_token_names.push_back("sin");
 }
 
 bool TokenManager::is_valid_name(const std::string &name) const noexcept {
-    return (this->available_tokens.count(name) == 0) ? false : true;
-}
+    bool exists = (std::find(
+        this->available_token_names.begin(),
+        this->available_token_names.end(),
+        name
+    ) != this->available_token_names.end());
 
-const std::string &TokenManager::get_name(const std::string &name) const {
-    if (this->is_valid_name(name)) {
-        return this->available_tokens.at(name);
-    }
-    else {
-        throw std::runtime_error("Token error: no tokens with such symbols available");
-    }
+    return exists;
 }
 
 const std::string &TokenManager::get_type_name(TokenType type) const {
