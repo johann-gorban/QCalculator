afeff590ed9fa0e0dbe767bc6ab40aa57a951498
der-johanni <ivangorbanwork@gmail.com>
2025-05-12 22:47:32 +0300

    switched to new commands

--- source/computation/calculator.cpp
+++ source/computation/calculator.cpp
@@ -7,10 +7,7 @@
 double Calculator::calculate(const std::vector<command_ptr> &commands){
     // Stack for computation. There the intermediate calculations are being stored
     std::stack<double> computations;
-
-    // Command manager for getting command for calculation by token
-    CommandManager command_manager;
-
+    
     for (const auto &command : commands) {
         command->execute(computations);
     } 
