38b83f6f30ad5d82f54be5edfba9bf76c2a4d379
der-johanni <ivangorbanwork@gmail.com>
2025-05-15 22:42:39 +0300

    Bug fixed

--- source/parsing/states.cpp
+++ source/parsing/states.cpp
@@ -1,28 +1,27 @@
 #include <stdexcept>
-
 #include "parsing/states.hpp"
-
+#include "token_manager.hpp"
 
 void ParseStartState::handle_token(ParserContext& context, token_ptr token) {
     state_ptr next_state = std::make_shared<ParseEndState>();
-    token_ptr extended_token(nullptr);
-
     TokenType token_type = token->get_type();
     const std::string token_data = token->get_data();
-
+    
     if (token_type == TokenType::Operator) {
-        extended_token = std::make_shared<OperatorToken>(*token, 0, Associativity::Right, Arity::Unary);
+        TokenManager token_manager;
+        unsigned int precedance = token_manager.get_precedance(token->get_data(), Arity::Unary);
+        token_ptr extended_token = std::make_shared<OperatorToken>(*token, precedance, Associativity::Right, Arity::Unary);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseUnaryOperatorState>();
     }
     else if (token_type == TokenType::Number) {
-        extended_token = std::make_shared<NumberToken>(*token);
+        token_ptr extended_token = std::make_shared<NumberToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseNumberState>();
     }
     else if (token_type == TokenType::Parenthesis) {
         if (token_data == "LEFT_PARENTHESIS") {
-            extended_token = std::make_shared<ParenthesisToken>(*token);
+            token_ptr extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
             next_state = std::make_shared<ParseLeftParenthesisState>();
         }
@@ -31,7 +30,7 @@ void ParseStartState::handle_token(ParserContext& context, token_ptr token) {
         }
     }
     else if (token_type == TokenType::Function) {
-        extended_token = std::make_shared<FunctionToken>(*token);
+        token_ptr extended_token = std::make_shared<FunctionToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseFunctionState>();
     }
@@ -41,53 +40,50 @@ void ParseStartState::handle_token(ParserContext& context, token_ptr token) {
     else {
         throw std::runtime_error("Syntax error: unknown token type");
     }
-
     context.set_state(next_state);
 }
 
 void ParseNumberState::handle_token(ParserContext& context, token_ptr token) {
     state_ptr next_state = std::make_shared<ParseEndState>();
-    token_ptr extended_token(nullptr);
-
     TokenType token_type = token->get_type();
 
     if (token_type == TokenType::Number) {
         throw std::runtime_error("Syntax error: a number cannot follow after other numbers");
     }
     else if (token_type == TokenType::Operator) {
-        extended_token = std::make_shared<OperatorToken>(*token, 0, Associativity::Left, Arity::Binary);
+        TokenManager token_manager;
+        unsigned int precedance = token_manager.get_precedance(token->get_data(), Arity::Binary);
+        token_ptr extended_token = std::make_shared<OperatorToken>(*token, precedance, Associativity::Left, Arity::Binary);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseBinaryOperatorState>();
     }
     else if (token_type == TokenType::Separator) {
-        extended_token = std::make_shared<SeparatorToken>(*token);
+        token_ptr extended_token = std::make_shared<SeparatorToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseSeparatorState>();
     }
     else if (token_type == TokenType::Function) {
-        extended_token = std::make_shared<FunctionToken>(*token);
+        token_ptr extended_token = std::make_shared<FunctionToken>(*token);
         context.append_extended_token(extended_token);
-
         const Token &multiplication_token = Token("*", TokenType::Operator);
         extended_token = std::make_shared<OperatorToken>(multiplication_token, 0, Associativity::Left, Arity::Binary);
         context.append_extended_token(extended_token);
-
         next_state = std::make_shared<ParseFunctionState>();
     }
     else if (token_type == TokenType::Parenthesis) {
         const std::string token_data = token->get_data();
         if (token_data == "LEFT_PARENTHESIS") {
+            const Token &multiplication_token = Token("*", TokenType::Operator);
+            token_ptr extended_token = std::make_shared<OperatorToken>(multiplication_token, 0, Associativity::Left, Arity::Binary);
+            context.append_extended_token(extended_token);
+
             extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
-
-            const Token &multiplication_token = Token("*", TokenType::Operator);
-            extended_token = std::make_shared<OperatorToken>(multiplication_token, 0, Associativity::Left, Arity::Binary);
-            context.append_extended_token(extended_token);
-
+            
             next_state = std::make_shared<ParseLeftParenthesisState>();
         }
         else if (token_data == "RIGHT_PARENTHESIS") {
-            extended_token = std::make_shared<ParenthesisToken>(*token);
+            token_ptr extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
             next_state = std::make_shared<ParseRightParenthesisState>();
         }
@@ -98,18 +94,15 @@ void ParseNumberState::handle_token(ParserContext& context, token_ptr token) {
     else {
         throw std::runtime_error("Syntax error: unknown token type");
     }
-
     context.set_state(next_state);
 }
 
 void ParseBinaryOperatorState::handle_token(ParserContext& context, token_ptr token) {
     state_ptr next_state = std::make_shared<ParseEndState>();
-    token_ptr extended_token(nullptr);
-
     TokenType token_type = token->get_type();
 
     if (token_type == TokenType::Number) {
-        extended_token = std::make_shared<NumberToken>(*token);
+        token_ptr extended_token = std::make_shared<NumberToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseNumberState>();
     }
@@ -120,14 +113,14 @@ void ParseBinaryOperatorState::handle_token(ParserContext& context, token_ptr to
         throw std::runtime_error("Syntax error: separator cannot follow after operator");
     }
     else if (token_type == TokenType::Function) {
-        extended_token = std::make_shared<FunctionToken>(*token);
+        token_ptr extended_token = std::make_shared<FunctionToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseFunctionState>();
     }
     else if (token_type == TokenType::Parenthesis) {
         const std::string token_data = token->get_data();
         if (token_data == "LEFT_PARENTHESIS") {
-            extended_token = std::make_shared<ParenthesisToken>(*token);
+            token_ptr extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
             next_state = std::make_shared<ParseLeftParenthesisState>();
         }
@@ -141,30 +134,27 @@ void ParseBinaryOperatorState::handle_token(ParserContext& context, token_ptr to
     else {
         throw std::runtime_error("Syntax error: unknown token type");
     }
-
     context.set_state(next_state);
 }
 
 void ParseUnaryOperatorState::handle_token(ParserContext& context, token_ptr token) {
     state_ptr next_state = std::make_shared<ParseEndState>();
-    token_ptr extended_token(nullptr);
-
     TokenType token_type = token->get_type();
 
     if (token_type == TokenType::Number) {
-        extended_token = std::make_shared<NumberToken>(*token);
+        token_ptr extended_token = std::make_shared<NumberToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseNumberState>();
     }
     else if (token_type == TokenType::Function) {
-        extended_token = std::make_shared<FunctionToken>(*token);
+        token_ptr extended_token = std::make_shared<FunctionToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseFunctionState>();
     }
     else if (token_type == TokenType::Parenthesis) {
         const std::string token_data = token->get_data();
         if (token_data == "LEFT_PARENTHESIS") {
-            extended_token = std::make_shared<ParenthesisToken>(*token);
+            token_ptr extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
             next_state = std::make_shared<ParseLeftParenthesisState>();
         }
@@ -181,20 +171,17 @@ void ParseUnaryOperatorState::handle_token(ParserContext& context, token_ptr tok
     else {
         throw std::runtime_error("Syntax error: unknown token type");
     }
-
     context.set_state(next_state);
 }
 
 void ParseFunctionState::handle_token(ParserContext& context, token_ptr token) {
     state_ptr next_state = std::make_shared<ParseEndState>();
-    token_ptr extended_token(nullptr);
-
     TokenType token_type = token->get_type();
 
     if (token_type == TokenType::Parenthesis) {
         const std::string token_data = token->get_data();
         if (token_data == "LEFT_PARENTHESIS") {
-            extended_token = std::make_shared<ParenthesisToken>(*token);
+            token_ptr extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
             next_state = std::make_shared<ParseLeftParenthesisState>();
         }
@@ -211,14 +198,11 @@ void ParseFunctionState::handle_token(ParserContext& context, token_ptr token) {
     else {
         throw std::runtime_error("Syntax error: unknown token type");
     }
-
     context.set_state(next_state);
 }
 
 void ParseSeparatorState::handle_token(ParserContext& context, token_ptr token) {
     state_ptr next_state = std::make_shared<ParseEndState>();
-    token_ptr extended_token(nullptr);
-
     TokenType token_type = token->get_type();
 
     if (token_type == TokenType::Separator) {
@@ -228,19 +212,19 @@ void ParseSeparatorState::handle_token(ParserContext& context, token_ptr token) 
         throw std::runtime_error("Syntax error: operator (unary/binary) cannot follow after separator");
     }
     else if (token_type == TokenType::Number) {
-        extended_token = std::make_shared<NumberToken>(*token);
+        token_ptr extended_token = std::make_shared<NumberToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseNumberState>();
     }
     else if (token_type == TokenType::Function) {
-        extended_token = std::make_shared<FunctionToken>(*token);
+        token_ptr extended_token = std::make_shared<FunctionToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseFunctionState>();
     }
     else if (token_type == TokenType::Parenthesis) {
         const std::string token_data = token->get_data();
         if (token_data == "LEFT_PARANTHESIS") {
-            extended_token = std::make_shared<ParenthesisToken>(*token);
+            token_ptr extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
             next_state = std::make_shared<ParseLeftParenthesisState>();
         }
@@ -254,38 +238,38 @@ void ParseSeparatorState::handle_token(ParserContext& context, token_ptr token) 
     else {
         throw std::runtime_error("Syntax error: unknown token type");
     }
-
     context.set_state(next_state);
 }
 
 void ParseLeftParenthesisState::handle_token(ParserContext& context, token_ptr token) {
     state_ptr next_state = std::make_shared<ParseEndState>();
-    token_ptr extended_token(nullptr);
-
     TokenType token_type = token->get_type();
 
     if (token_type == TokenType::Separator) {
         throw std::runtime_error("Syntax error: separator cannot follow after left parenthesis");
     }
     else if (token_type == TokenType::Operator) {
-        extended_token = std::make_shared<OperatorToken>(*token, 0, Associativity::Right, Arity::Unary);
+        TokenManager token_manager;
+        unsigned int precedance = token_manager.get_precedance(token->get_data(), Arity::Unary);
+
+        token_ptr extended_token = std::make_shared<OperatorToken>(*token, precedance, Associativity::Right, Arity::Unary);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseUnaryOperatorState>();
     }
     else if (token_type == TokenType::Number) {
-        extended_token = std::make_shared<NumberToken>(*token);
+        token_ptr extended_token = std::make_shared<NumberToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseNumberState>();
     }
     else if (token_type == TokenType::Function) {
-        extended_token = std::make_shared<FunctionToken>(*token);
+        token_ptr extended_token = std::make_shared<FunctionToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseFunctionState>();
     }
     else if (token_type == TokenType::Parenthesis) {
         const std::string token_data = token->get_data();
         if (token_data == "LEFT_PARANTHESIS") {
-            extended_token = std::make_shared<ParenthesisToken>(*token);
+            token_ptr extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
             next_state = std::make_shared<ParseLeftParenthesisState>();
         }
@@ -299,23 +283,22 @@ void ParseLeftParenthesisState::handle_token(ParserContext& context, token_ptr t
     else {
         throw std::runtime_error("Syntax error: unknown token type");
     }
-
     context.set_state(next_state);
 }
 
 void ParseRightParenthesisState::handle_token(ParserContext& context, token_ptr token) {
     state_ptr next_state = std::make_shared<ParseEndState>();
-    token_ptr extended_token(nullptr);
-
     TokenType token_type = token->get_type();
 
     if (token_type == TokenType::Separator) {
-        extended_token = std::make_shared<SeparatorToken>(*token);
+        token_ptr extended_token = std::make_shared<SeparatorToken>(*token);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseSeparatorState>();
     }
     else if (token_type == TokenType::Operator) {
-        extended_token = std::make_shared<OperatorToken>(*token, 0, Associativity::Left, Arity::Binary);
+        TokenManager token_manager;
+        unsigned int precedance = token_manager.get_precedance(token->get_data(), Arity::Binary);
+        token_ptr extended_token = std::make_shared<OperatorToken>(*token, precedance, Associativity::Left, Arity::Binary);
         context.append_extended_token(extended_token);
         next_state = std::make_shared<ParseBinaryOperatorState>();
     }
@@ -328,17 +311,20 @@ void ParseRightParenthesisState::handle_token(ParserContext& context, token_ptr 
     else if (token_type == TokenType::Parenthesis) {
         const std::string token_data = token->get_data();
         if (token_data == "LEFT_PARENTHESIS") {
-            extended_token = std::make_shared<ParenthesisToken>(*token);
+            token_ptr extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
-
             const Token &multiplication_token = Token("*", TokenType::Operator);
-            extended_token = std::make_shared<OperatorToken>(multiplication_token, 0, Associativity::Left, Arity::Binary);
+
+            TokenManager token_manager;
+            unsigned int precedance = token_manager.get_precedance("*", Arity::Binary);
+
+            extended_token = std::make_shared<OperatorToken>(multiplication_token, precedance, Associativity::Left, Arity::Binary);
             context.append_extended_token(extended_token);
 
             next_state = std::make_shared<ParseLeftParenthesisState>();
         }
         else if (token_data == "RIGHT_PARENTHESIS") {
-            extended_token = std::make_shared<ParenthesisToken>(*token);
+            token_ptr extended_token = std::make_shared<ParenthesisToken>(*token);
             context.append_extended_token(extended_token);
             next_state = std::make_shared<ParseRightParenthesisState>();
         }
@@ -349,7 +335,6 @@ void ParseRightParenthesisState::handle_token(ParserContext& context, token_ptr 
     else {
         throw std::runtime_error("Syntax error: unknown token type");
     }
-
     context.set_state(next_state);
 }
 
