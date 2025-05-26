fbf77f5f8294df2874c4576d0aa46998c3813e03
johann-gorban <ivangorbanwork@gmail.com>
2025-05-26 21:04:29 +0300

    history redesign

--- calculation/include/history/history.hpp
+++ calculation/include/history/history.hpp
@@ -2,19 +2,15 @@
 
 #include "tokenization/tokens.hpp"
 
-#include <unordered_map>
 #include <vector>
 #include <string>
 
 class History {
 private:
-    std::size_t last_id;
-    std::unordered_map<std::size_t, std::vector<token_ptr>> token_table;
-    std::unordered_map<std::size_t, std::string> result_table;
+    std::vector<std::string> expressions;
 public:
-    void save_calculation(const std::vector<token_ptr> &tokens, const std::string &result) noexcept;
+    void save(const std::string &expression) noexcept;
 
-    const std::vector<token_ptr> get_tokens(const std::size_t id) const;
-
-    const std::string get_result(const std::size_t id) const;
+    // Returns empty string if invalid index given
+    const std::string get(const std::size_t &index);
 };
--- calculation/source/history/history.cpp
+++ calculation/source/history/history.cpp
@@ -2,26 +2,14 @@
 
 #include <stdexcept>
 
-void History::save_calculation(const std::vector<token_ptr> &tokens, const std::string &result) noexcept {
-    this->last_id++;
-    this->token_table[last_id] = tokens;
-    this->result_table[last_id] = result;
-};
-
-const std::vector<token_ptr> History::get_tokens(const std::size_t id) const {
-    if (this->token_table.count(id)) {
-        return this->token_table.at(id);
-    }
-    else {
-        throw std::runtime_error("History does not have tokens for expression with ID:" + std::to_string(id));
-    }
+void History::save(const std::string &expression) noexcept {
+    this->expressions.push_back(expression);
 }
 
-const std::string History::get_result(const std::size_t id) const {
-    if (this->result_table.count(id)) {
-        return this->result_table.at(id);
+const std::string History::get(const std::size_t &index) {
+    std::string result = "";
+    if (index < this->expressions.size()) {
+        result = this->expressions[index];
     }
-    else {
-        throw std::runtime_error("History does not have result for expression with ID:" + std::to_string(id));
-    }
+    return result;
 }
